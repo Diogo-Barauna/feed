1b989c11de78df38cb16559c40be3dec9bf7c163
Diogo <diogobarauna233@gmail.com>
2023-05-09 17:52:55 -0300

    Adicionando a imagem principal exibida no feed e a logo 'instagram' temporária

--- App.js
+++ App.js
@@ -1,32 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.body}>
-      <View style={styles.container}>
-        <Text>Feed</Text>
-        
-        <StatusBar style="auto"/>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.logo}>Instagram</Text>
       </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('./img.jpg')}
+          style={styles.image}
+        />
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  body:{
-    width: '100%',
-    backgroundColor: '#C1C4CA'
-  },
   container: {
     marginLeft: '265px',
     marginTop:'20px',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     width: '200px',
     height: '350px',
     border: 'solid',
-    borderRadius: '10px'
+    borderRadius: '10px',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    position: 'absolute',
+    top: 80,
+    left: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  image: {
+    width: 198,
+    height: 170,
+  },
+  logo: {
+    fontSize: 13,
+    fontStyle: 'italic',
   },
 });
