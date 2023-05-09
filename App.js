f8cfdc876665fc020e22c326d3eb018dc559b2a0
Diogo <diogobarauna233@gmail.com>
2023-05-09 16:57:53 -0300

    Iniciando a estilização

--- App.js
+++ App.js
@@ -3,18 +3,30 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <Text>Feed</Text>
+        
+        <StatusBar style="auto"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  body:{
+    width: '100%',
+    backgroundColor: '#C1C4CA'
+  },
   container: {
-    flex: 1,
+    marginLeft: '265px',
+    marginTop:'20px',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '200px',
+    height: '350px',
+    border: 'solid',
+    borderRadius: '10px'
   },
 });
